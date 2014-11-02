6750f1fb4abb0c4ea59eeb784785c56f588ffa2f
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-02 22:07:56 +0500

    BLADE IS AWESOME *__*

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -1,13 +1,14 @@
 <!doctype html>
 <html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Dashboard</title>
-</head>
-<body>
-  <h1>Hello World!</h1>
-  @for($i=0;$i<10;$i++){
-    <p>Umayr is Awesome</p>
-  }
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        @for($i=0;$i<10;$i++)
+            <p>Umayr is Awesome</p>
+        @endfor
+    </body>
 </html>
