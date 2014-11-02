b8eafc149d8a3e38b89f2f0793ea8cf4d26cc250
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-02 22:05:47 +0500

    Ad & User models :3

--- app/models/Ad.php
+++ app/models/Ad.php
@@ -5,6 +5,50 @@
  * Time     : 8:11 PM
  */
 
-class Ad {
+use Rhumsaa\Uuid\Exception\UnsatisfiedDependencyException;
+use Rhumsaa\Uuid\Uuid;
+use Illuminate\Database\Eloquent\Model;
 
+class Ad extends Model
+{
+    public $id;
+    public $name;
+    public $height;
+    public $width;
+    public $added_on;
+    public $unique_id;
+
+    protected $table = 'ads';
+
+    public function Ad($name, $size)
+    {
+        $this->name = $name;
+        $this->height = explode("x", $size)[0];
+        $this->width = explode("x", $size)[1];
+        $date = new DateTime();
+        $this->added_on = $date->format('c');
+        $this->unique_id = $this->getUniqueId();
+    }
+
+    protected function getUniqueId()
+    {
+        try {
+            return Uuid::uuid1();
+        } catch (UnsatisfiedDependencyException $e) {
+            throw new Exception($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function insert()
+    {
+        return DB::table($this->table)->insertGetId(
+            array(
+                'name' => $this->name,
+                'height' => $this->height,
+                'width' => $this->width,
+                'added_on' => $this->added_on,
+                'unique_id' => $this->unique_id
+            )
+        );
+    }
 } 
