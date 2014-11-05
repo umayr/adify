bd1e90794234a1125cd5a90fcbb5f1480ba770bf
Umayr Shahid <umayrr@hotmail.co.uk>
2014-11-05 22:40:27 +0500

    routes for plugs

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,14 @@ Route::get('/dashboard', 'DashboardController@index');
 Route::post('/ad/create', 'AdController@create');
 Route::get('/ad/all', 'AdController@all');
 Route::get('/ad/available-sizes', 'AdController@availableSizes');
+
+Route::get('/plug', 'PlugController@index');
+Route::get('/plug/{unique_id}', 'PlugController@plugger');
+
+Route::post('/plug/click', function () {
+    return Input::get('unique_id');
+});
+
+Route::post('/plug/hover', function () {
+    return Input::get('time');
+});
